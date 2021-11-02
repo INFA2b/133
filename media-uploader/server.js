ac91869cba6085f188053f006d4ec70a6385b180
Daniel Lutz <daniel.lutz@mobilezone.ch>
2021-11-02 15:58:42 +0100

    Move mongodb implementation into server js

--- media-uploader/server.js
+++ media-uploader/server.js
@@ -1,12 +1,28 @@
+// Dependencies implementation
 const express = require('express')
 const app = express()
 const port = 3000
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/contentdb";
 
+// Initialize database
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("images", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+
+// Define server directory
 app.use(express.static(__dirname));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
+// Serve on localhost and port {port}
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
