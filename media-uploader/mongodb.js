bd89da5c69cacaaddaec2bcebfa14a9dbd0638ce
Daniel Lutz <daniel.lutz@mobilezone.ch>
2021-11-02 15:49:01 +0100

    Change mongodb js to add collection

--- media-uploader/mongodb.js
+++ media-uploader/mongodb.js
@@ -3,6 +3,10 @@ const url = "mongodb://localhost:27017/contentdb";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
-  db.close();
+  var dbo = db.db("mydb");
+  dbo.createCollection("images", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
 });
